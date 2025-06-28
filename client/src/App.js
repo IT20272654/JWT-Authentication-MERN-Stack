ce33d65241726cefd1a682457885b738c34f7f9f
Shavindu Rajapaksha <shavindurajapaksha@gmail.com>
2025-06-28 20:50:28 +0530

    Updated App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,45 +1,18 @@
-import './App.css';
-import React, { useState } from 'react';
+import React from 'react'
 
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Register from './pages/Register'
+import Login from './pages/Login'
 
 function App() {
-
-  const [Username, setUsername] = useState('');
-  const [Email, setEmail] = useState('');
-  const [Password, setPassword] = useState('');
-
-  const registerUser = async (e) => {
-    e.preventDefault();
-    const response = await fetch("http://localhost:8000/api/register", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        username: Username,
-        email: Email,
-        password: Password,
-      }),
-    });
-    const data = await response.json();
-    console.log(data);
-  };
-
   return (
-    <div className="App">
-      <h1>Register</h1>
-      <form onSubmit={registerUser}>
-        <input type="text" placeholder="Username" value={Username} onChange={(e) => setUsername(e.target.value)} />
-        <br /><br />
-        <input type="email" placeholder="Email" value={Email} onChange={(e) => setEmail(e.target.value)} />
-        <br /><br />
-        <input type="password" placeholder="Password" value={Password} onChange={(e) => setPassword(e.target.value)} />
-        <br /><br />
-        <button type="submit">Register</button>
-      </form>
-    </div>
-  );
+    <Router>
+      <Routes>
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </Router>
+  )
 }
 
 export default App;
