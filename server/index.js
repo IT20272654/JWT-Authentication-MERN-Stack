638297060d33915fc95983acbf07bf404a652e05
Shavindu Rajapaksha <shavindurajapaksha@gmail.com>
2025-06-28 15:25:20 +0530

    Updated server index.js

--- server/index.js
+++ server/index.js
@@ -1,10 +1,32 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
+const mongoose = require('mongoose');
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.use(cors());
+app.use(express.json());
+
+app.post('/api/register', (req, res) => {
+  const { username, email, password } = req.body;
+
+  // Here you would typically handle the registration logic, e.g., saving the user to a database
+  console.log('User registered:', { username, email, password });
+  res.status(201).json({ message: 'User registered successfully' });
 });
+
 
 app.listen(8000, () => {
   console.log('Server is running on port:8000');
 });
+
+
+
+
+
+
+
+
+
+
+
+
